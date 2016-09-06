da6878f33480f1bbff6709301d3c340c5353dd64
Nick Field <heyimnick@live.com>
2016-09-06 09:45:30 +0100

    Indented text

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ var unitsButton = document.getElementById("units-button");
 function getJSON(url, callback) {
 
     var xhr = new XMLHttpRequest();
+
     xhr.open('GET', url, true);
 
     xhr.onload = function(){
+
         if (xhr.status >= 200 && xhr.status < 400) {
             var data = JSON.parse(xhr.responseText);
             callback(data);
+
         } else {
             callback("error")
         }
@@ -26,11 +29,14 @@ function getJSON(url, callback) {
 
 
 var jsonPromise = new Promise(function(resolve,reject){
+
     getJSON('http://ipinfo.io/json', function(data) {
+
         if (data != "error") {
             dataObject.city = data.city;
             dataObject.country = data.country;
             resolve();
+
         } else {
             reject(Error("Unsuccessful"));
         }
@@ -38,14 +44,24 @@ var jsonPromise = new Promise(function(resolve,reject){
 )})
 
     .then(function(){
-        var openWeatherMapUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
-        var APPID = '9b9d29dd1bd49a8456bdd3d33021da80';
-        var weatherUrl = openWeatherMapUrl + dataObject.city + ',' + dataObject.country + '&APPID=' + APPID + '&units=imperial';
+
+        var URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+        var APPID = '&APPID=9b9d29dd1bd49a8456bdd3d33021da80';
+        var UNITS = '&units=imperial'
+        var weatherUrl =
+            URL
+            + dataObject.city
+            + dataObject.country
+            + APPID
+            + UNITS;
+
         getJSON(weatherUrl, function(data) {
+
             if (data != "error") {
                 dataObject.temp = data.main.temp;
                 dataObject.desc = data.weather[0].main;
                 updateDom("celcius");
+
             } else {
                 console.log('error');
             }
@@ -53,30 +69,38 @@ var jsonPromise = new Promise(function(resolve,reject){
     })
 
 function updateDom(units){
+
         var temp;
+
         if (units == 'fahrenheit') {
             temp = `${(dataObject.temp).toFixed(0)}°`;
+
         } else {
             temp = `${((dataObject.temp - 32) * 0.5555).toFixed(0)}°`;
         }
+
         locationElement.innerHTML = `${dataObject.city}, ${dataObject.country}`;
         tempElement.innerHTML = temp;
         descElement.innerHTML = `'${dataObject.desc}'`;
 }
 
 
-unitsButton.addEventListener("click",function(){
+unitsButton.addEventListener("click", function(){
+
     if (unitsButton.innerHTML == "C") {
+
         unitsButton.innerHTML = "F";
         updateDom("fahrenheit");
+
     } else {
+
         unitsButton.innerHTML = "C";
         updateDom("celcius");
     }
 })
 
 
-rainMaker("text-canvas", 24, 8, 4, 100);
+rainMaker("text-canvas", 24, 8, 10, 50);
 
 
 
